6e83e5f515bb7d13658b74ac38d20113ae831e39
Official@ Vedansh Yadav <83397824+Official-Vedansh-Yadav@users.noreply.github.com>
2021-07-07 06:54:02 +0530

    files are addes

---
+++ main.js
@@ -0,0 +1,29 @@
+// let screen = document.getElementById('screen');
+// buttons = document.querySelectorAll('button');
+// let screenValue = '';
+//      for(item of buttons){
+//     item.addEventListener('click', (e)=>{
+//         buttonText = e.target.innerText;
+//         console.log('button text', buttonText);
+//         if (buttonText=='X') {
+//             buttonText = '*';
+//             screenValue += buttonText ;
+//             forms.answer.value = screenValue;
+//         } else if (buttonText == 'c') {
+//             screenValue = '';
+//             forms.answer.value = screenValue;
+//         } else if (buttonText == '=') {
+//             screenValue = eval(forms.answer.value) ;
+//             forms.answer.value = screenValue;
+//         } else{
+//             screenValue += buttonText;
+//             forms.answer.value = screenValue;
+//         }
+//     })
+// }
+
+function back(){
+    let value = forms.answer.value;
+    value = value.substr(0,value.length-1);
+    forms.answer.value = value;
+}
